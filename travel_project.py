cc26d0ee3004277285245f83a50e3d23b66ee5bf
nitinarya2001 <85936367+nitinarya2001@users.noreply.github.com>
2022-10-11 18:23:21 +0530

    Update travel_project.py

--- travel_project.py
+++ travel_project.py
@@ -1,17 +1,21 @@
-import requests
-import json
-
-r = requests.get(f"http://router.project-osrm.org/route/v1/car/{lon_1},{lat};{lon_2},{lat_2}?overview=false""")
-
-routes = json.loads(r.content)
-route_1 = routes.get("routes")[0]
-
 print("Enter List of Places you want to visit : ")
 t = list(input().split(' '))
 
 for i in t:
   print(i)
-  
 
 
+import requests, json
+ 
+api_key = 'Your_API_key'
+url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+query = input('Search query: ')
   
+r = requests.get(url + 'query=' + query +
+                        '&key=' + api_key)
+  
+x = r.json()
+y = x['results']
+
+for i in range(len(y)):
+    print(y[i]['name'])
